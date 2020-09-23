a40a07b053f5d5cde38f1bdad66d4d7870a22954
Janssen Segura <seguragjj25@gmail.com>
2020-09-22 19:27:26 -0500

    Preparing workspace and routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArtistComponent } from './components/artist/artist.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// *HttpsModule 
+import { HttpClientModule } from '@angular/common/http';
 
+// *Routes
 import { AppRoutingModule } from './app-routing.module';
+// *Components 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArtistComponent } from './components/artist/artist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    NavBarComponent,
+    HomeComponent,
+    ArtistComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
